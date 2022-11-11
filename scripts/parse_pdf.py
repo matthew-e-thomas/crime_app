2e77fe4db57b8e792b2506a40b1a7841f00394ef
matthew-e-thomas <matt.thomas@email.virginia.edu>
2022-11-11 12:42:07 -0500

    update DATE regex

--- scripts/parse_pdf.py
+++ scripts/parse_pdf.py
@@ -20,17 +20,18 @@ def extract_components(file_path: Path = DATA_PATH / 'crime_report_8.2.2022.pdf'
     text = parse_pdf(file_path)
     CATEGORY_RE = re.compile(r'.*(?=:)')
     ADDRESS_RE = re.compile(r'\d{1,5}[\s\w]+(?=,\s\d)')
-    DATE_RE = re.compile(r'\d{1,2}\/\d{1,2}')
+    DATE_RE = re.compile(r'(\d{1,2}\/\d{1,2}).{3}(\d{1,2}\/\d{1,2})?')
     TEXT_AFTER_DATE_RE = re.compile(r"(?<=\d,)\s*([^\.]*)")
     text_reduced = re.sub(r'Arrests:((.|\n)*)', '', text)   # remove arrests
-    text_reduced = re.sub(r'(Crime.*)', '', text_reduced)   # remove arrests
+    text_reduced = re.sub(r'(Crime.*)', '', text_reduced)   # remove header Crime Report
     addresses = ADDRESS_RE.findall(text_reduced)
     dates = DATE_RE.findall(text_reduced)
     descriptions = TEXT_AFTER_DATE_RE.findall(text_reduced)
     descriptions = [text.replace('\n', '') for text in descriptions]
     categories = CATEGORY_RE.findall(text_reduced)
     report_dict = {'category': categories, 'addresses': addresses, 'dates': dates, 'descriptions': descriptions}
-    print(report_dict)
+    print (len(dates), len(addresses), len(descriptions), len(categories))
+    print(addresses)
     return report_dict
 
 
