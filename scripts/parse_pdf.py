7168a4b6a25be0d099eca25280bee2ce77ff0b7b
matthew-e-thomas <matt.thomas@email.virginia.edu>
2022-11-21 10:45:03 -0500

    got matching counts on parsed reports

--- scripts/parse_pdf.py
+++ scripts/parse_pdf.py
@@ -2,6 +2,7 @@ from pdfminer.high_level import extract_text
 from pathlib import Path
 import re
 import typer
+from collections import defaultdict
 
 DATA_PATH = Path('../data')
 
@@ -28,6 +29,7 @@ def extract_components(file_path: Path = DATA_PATH / 'crime_report_8.2.2022.pdf'
     headers = re.findall(CATEGORY_RE, text_reduced)         # find headers
     cleaned_headers = [header.strip() for header in headers if header != '']
     header_count = len(cleaned_headers)
+    report_dict = defaultdict(list)
     for i, text in enumerate(cleaned_headers):
         header_count -= 1
         if header_count > 0:
@@ -35,16 +37,16 @@ def extract_components(file_path: Path = DATA_PATH / 'crime_report_8.2.2022.pdf'
             main_body = re.search(regex_string, text_reduced).group()
         else:
             main_body = re.search(re.escape(cleaned_headers[i]) + r'(.|\n)*', text_reduced).group()
-        print(main_body)
-    # addresses = ADDRESS_RE.findall(text_reduced)
-    # dates = DATE_RE.findall(text_reduced)
-    # descriptions = TEXT_AFTER_DATE_RE.findall(text_reduced)
-    # descriptions = [text.replace('\n', '') for text in descriptions]
-    # categories = CATEGORY_RE.findall(text_reduced)
-    # report_dict = {'category': categories, 'addresses': addresses, 'dates': dates, 'descriptions': descriptions}
-    # print (len(dates), len(addresses), len(descriptions), len(categories))
-    # print(addresses)
-    # return report_dict
+        addresses = ADDRESS_RE.findall(main_body)
+        dates = DATE_RE.findall(main_body)
+        descriptions = TEXT_AFTER_DATE_RE.findall(main_body)
+        descriptions = [text.replace('\n', '') for text in descriptions]
+        category = CATEGORY_RE.match(main_body)
+        category_dict = {'category': category.group(), 'addresses': addresses, 'dates': dates, 'descriptions': descriptions}
+        for key, value in category_dict.items():
+            report_dict[key].append(value)
+    print(len(report_dict['category']), len(report_dict['addresses']), len(report_dict['dates']), len(report_dict['descriptions']))
+    return report_dict
 
 
 if __name__ == '__main__':
