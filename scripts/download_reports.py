7262a676c720c9646e1feb161aa6052c131ab6bf
matthew-e-thomas <matt.thomas@email.virginia.edu>
2023-02-10 15:29:37 -0500

    remove redundant code

--- scripts/download_reports.py
+++ scripts/download_reports.py
@@ -27,30 +27,3 @@ def download_pdfs(url: str = POLICE_URL) -> None:
 
 if __name__ == '__main__':
     typer.run(download_pdfs)
-
-# response = requests.get(url)
-
-
-# soup = BeautifulSoup(response.text, 'html.parser')
-# Find all hyperlinks present on webpage
-# links = soup.find_all('a')
-#
-# i = 0
-#
-# # From all links check for pdf link and
-# # if present download file
-# for link in links:
-#     if ('.pdf' in link.get('href', [])):
-#         i += 1
-#         print("Downloading file: ", i)
-#
-#         # Get response object for link
-#         response = requests.get(link.get('href'))
-#
-#         # Write content in pdf file
-#         pdf = open("pdf"+str(i)+".pdf", 'wb')
-#         pdf.write(response.content)
-#         pdf.close()
-#         print("File ", i, " downloaded")
-#
-# print("All PDF files downloaded")
